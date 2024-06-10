cd2d6192258f684b63148e2936caaff92f537ddf
Hugo Pessoa <hphugopessoa@outlook.com>
2024-06-10 19:42:42 -0300

    package.json upgrades e downgrades

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['react-native-reanimated/plugin'],
-    {
-      globals: ['__scanORC'],
-    }
-  ]
+    plugins: ['react-native-reanimated/plugin']
   };
 };
