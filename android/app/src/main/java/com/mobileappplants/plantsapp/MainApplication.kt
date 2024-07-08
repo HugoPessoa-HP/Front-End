ff60e58c3bee7915a6e3da73424bf990554c0ea8
Hugo Pessoa <hphugopessoa@outlook.com>
2024-07-08 19:51:58 -0300

    Modificações na Classificação 9

--- android/app/src/main/java/com/mobileappplants/plantsapp/MainApplication.kt
+++ android/app/src/main/java/com/mobileappplants/plantsapp/MainApplication.kt
@@ -2,7 +2,6 @@ package com.mobileappplants.plantsapp
 
 import android.app.Application
 import android.content.res.Configuration
-import com.ccf.BuildConfig
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
